21d88451b108feedf8f71d74ed0f88c3da06ee34
sampresser <presser@uwm.edu>
2022-03-23 05:28:15 -0400

    first commit

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <complex.h>
+#include <math.h>
+#define __STDC_WANT_LIB_EXT1__ 1
+
+
+
+
+
+int main()
+{
+    const double pi = 3.141592654; //cant be modified
+    const float *pf; //pf points to a constant float value
+
+    float * const pt; // pt is a constant pointer
+    const float * const ptr; //constant pointer to constant value
+
+   
+
+}
+
